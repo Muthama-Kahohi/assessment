af44cedaf36bbeb51af91af3b1e37026bd3deed3
Paul Kahohi <pkahohi@safaricom.co.ke>
2019-07-24 10:10:27 +0300

    Initial Commit introducing the project to Git

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pauuh on 24/07/2019.
+//  Copyright © 2019 Safaricom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
